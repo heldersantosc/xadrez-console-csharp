485d3748a061853c6e268287761d99f745332d0f
Helder Santos <heldersantos@bemol.onmicrosoft.com>
2022-03-13 16:16:10 -0400

    criando tela, pecas, posicao e tabuleiro
    Signed-off-by: Helder Santos <heldersantos@bemol.onmicrosoft.com>

--- Program.cs
+++ Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Text;
+using tabuleiro;
+using xadrex;
 
 namespace xadrez_console
 {
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = Encoding.UTF8;
+
+
+            Tabuleiro tab = new Tabuleiro(8, 8);
+
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+
+            Tela.imprimirTabuleiro(tab);
+
+            Console.ReadLine();
         }
     }
 }
---
+++ tabuleiro/Peca.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace tabuleiro
+{
+    internal class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int qtdMovimentos { get; protected set; }
+        public Tabuleiro tabuleiro { get; protected set; }
+
+        public Peca(Cor cor, Tabuleiro tabuleiro)
+        {
+            this.cor = cor;
+            this.posicao = null;
+            this.qtdMovimentos = 0;
+            this.tabuleiro = tabuleiro;
+        }
+    }
+}
